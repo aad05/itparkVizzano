5bc15aac0f76e3e944abdc19d1edfcd2314bc5cb
aad05 <abduvoitovasadbek3@gmail.com>
2023-01-20 16:03:30 +0500

    Some mistakes rectified

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -57,7 +57,7 @@ const Navbar = () => {
               }}
               size="large"
             >
-              A
+              M
             </Avatar>
           </Dropdown>
         </Wrapper.Container>
